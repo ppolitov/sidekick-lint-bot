a64aa2096cf82c6a56600282f066036e040c263b
Pavel Politov <politov@meetsidekick.com>
2020-10-02 00:42:31 +0400

    ESlint works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { CLIEngine } = require('eslint')
 
-const jsFileFilter = ({ filename }) => filename.match(/.*(.js|.jsx)$/)
+const BOT_NAME = 'sidekick-lint-bot'
 
 function getCommentPositionMap(patchString) {
   let commentPosition = 0
@@ -31,22 +31,43 @@ async function getRepoFile(context, path, options) {
   const params = context.repo({ path })
   if (options)
     Object.assign(params, options)
-  const data = await context.github.repos.getContent(params)
-  return Buffer.from(data.content, 'base64').toString()
+  const response = await context.github.repos.getContent(params)
+  if (response.status === 200) {
+    return Buffer.from(response.data.content, 'base64').toString()
+  }
+  return ''
+}
+
+/**
+ * @param {Object} context
+ * @param {String} owner
+ * @param {String} repo
+ * @param {number} number
+ */
+async function removeOldReview(context, owner, repo, number) {
+  const { data } = await context.github.pulls.listReviews(
+    {owner, repo, pull_number: number})
+  data.forEach(async (review) => {
+    if (review.user.login.indexOf(BOT_NAME) === 0) {
+      await context.github.pulls.dismissReview(
+        {owner, repo, pull_number: number, review_id: review.id, message: 'Outdated.'})
+    }
+  })
 }
 
 /**
  * @param {Object} context
  */
 async function lint(context) {
-  const config = JSON.parse(await getRepoFile(context, '.eslintrc.json'))
+  const json = await getRepoFile(context, '.eslintrc.json')
+  const config = json !== '' ? JSON.parse(json) : {}
   const eslint = new CLIEngine({
     baseConfig: config,
     envs: ['es2020'],
     useEslintrc: false,
   })
 
-  const { action, pull_request: pullRequest, repository } = context.payload
+  const { pull_request: pullRequest, repository } = context.payload
   const { base, head, number } = pullRequest
   const [owner, repo] = repository.full_name.split('/')
 
@@ -56,51 +77,53 @@ async function lint(context) {
   }))
 
   const { files } = compare.data
-  const data = await Promise.all(files.filter(f => f.status !== 'removed').filter(jsFileFilter).map(async (file) => {
-    const content = await getRepoFile(context, file.filename, {
-      owner,
-      repo,
-      path: file.filename,
-      ref: head.sha
-    })
-
-    return {
-      filename: file.filename,
-      lineMap: getCommentPositionMap(file.patch),
-      content,
-    }
-  }))
+  const data = await Promise.all(files
+    .filter(f => f.status !== 'removed')
+    .filter(({ filename }) => filename.match(/.*(.js|.jsx)$/))
+    .map(async (file) => {
+      const content = await getRepoFile(context, file.filename, {
+        owner,
+        repo,
+        path: file.filename,
+        ref: head.sha
+      })
+      return {
+        filename: file.filename,
+        lineMap: getCommentPositionMap(file.patch),
+        content,
+      }
+    }))
 
   const comments = []
   data.forEach((data) => {
     const { results: [result] } = eslint.executeOnText(data.content, data.filename)
     const eslintErrors = result.messages
-    comments.push(...eslintErrors.filter(error => data.lineMap[error.line] && error.severity === 2).map(error => ({
-      path: data.filename,
-      position: data.lineMap[error.line],
-      body: `**${error.ruleId}**: ${error.message}`,
-    })))
+    comments.push(...eslintErrors
+      .map(error => ({
+        path: data.filename,
+        position: data.lineMap[error.line],
+        body: `**${error.ruleId}**: ${error.message}`,
+      })))
   })
-  console.log('Comments:', comments)
 
-  /*
   if (comments.length > 0) {
-    await context.github.pullRequests.createReview({
+    //await removeOldReview(context, owner, repo, number)
+
+    await context.github.pulls.createReview({
       owner,
       repo,
-      number,
-      comments,
+      pull_number: number,
       event: 'REQUEST_CHANGES',
-      body: 'ESLint found some errors. Please fix them and try committing again.',
+      comments,
+      body: 'ESLint found some errors.',
     })
-  } */
+  }
 }
 
 /**
  * @param {import('probot').Application} app
  */
 module.exports = (app) => {
-  app.log.info('Yay, the app was loaded!')
   app.on('pull_request.opened', lint)
   app.on('pull_request.synchronize', lint)
 }
