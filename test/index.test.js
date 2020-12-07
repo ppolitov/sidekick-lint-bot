b96c2c843336475c4030a3a0aa7f1e718e3fa167
Pavel Politov <politov@meetsidekick.com>
2020-12-07 15:45:54 +0400

    fake test

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,15 @@
-const nock = require('nock')
+const nock = require('nock');
 // Requiring our app implementation
-const myProbotApp = require('..')
-const { Probot, ProbotOctokit } = require('probot')
+const myProbotApp = require('..');
+const { Probot, ProbotOctokit } = require('probot');
 // Requiring our fixtures
-const payload = require('./fixtures/issues.opened')
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
-const fs = require('fs')
-const path = require('path')
+const payload = require('./fixtures/issues.opened');
+const issueCreatedBody = { body: 'Thanks for opening this issue!' };
+const fs = require('fs');
+const path = require('path');
 
-const privateKey = fs.readFileSync(path.join(__dirname, 'fixtures/mock-cert.pem'), 'utf-8')
+const privateKey = fs.readFileSync(
+  path.join(__dirname, 'fixtures/mock-cert.pem'), 'utf-8');
 
 describe('My Probot app', () => {
   let probot
@@ -18,7 +19,6 @@ describe('My Probot app', () => {
     probot = new Probot({
       id: 123,
       privateKey,
-      // disable request throttling and retries for testing
       Octokit: ProbotOctokit.defaults({
         retry: { enabled: false },
         throttle: { enabled: false }
@@ -30,25 +30,16 @@ describe('My Probot app', () => {
 
   test('creates a comment when an issue is opened', async () => {
     const mock = nock('https://api.github.com')
-
-      // Test that we correctly return a test token
       .post('/app/installations/2/access_tokens')
-      .reply(200, {
-        token: 'test',
-        permissions: {
-          issues: 'write'
-        }
-      })
-
-      // Test that a comment is posted
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body) => {
+      .reply(200, {token: 'test', permissions: {issues: 'write'}})
+      .post('/repos/pushplaylabs/sidekick-ext-main/pull/1', (body) => {
         expect(body).toMatchObject(issueCreatedBody)
         return true
       })
       .reply(200)
 
     // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
+    await probot.receive({name: 'pull_request.opened', payload})
 
     expect(mock.pendingMocks()).toStrictEqual([])
   })
@@ -58,9 +49,3 @@ describe('My Probot app', () => {
     nock.enableNetConnect()
   })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
